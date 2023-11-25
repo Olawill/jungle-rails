14f02057fffbf6b0c1d92d397e09838fcce52b58
Olawale <henry.williams658@gmail.com>
2023-11-25 23:05:12 +0000

    Add Admin Dashboard Functionality

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,16 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_USERNAME'], password: ENV['AUTH_PASSWORD']
 
   def show
+    product_count = Product.count
+    category_count = Category.count
+    low_stock = Product.where('quantity = 0').count
+    total_order = Order.count
+
+    @data_hash = {
+      product_count: product_count,
+      category_count: category_count,
+      low_stock: low_stock,
+      total_order: total_order
+    }
   end
 end
