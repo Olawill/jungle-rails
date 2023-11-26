2d9d504602fd6e97826c16bbcb0e9da4061f32b9
Olawale <henry.williams658@gmail.com>
2023-11-26 02:30:07 +0000

    Add category route to admin

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,15 @@ class Category < ApplicationRecord
 
   has_many :products
 
+  validates :name, presence: true
+  validate :name_not_empty_string
+
+  private
+
+  def name_not_empty_string
+    if name.present? && name.strip.empty?
+      errors.add(:name, "can't be just spaces")
+    end
+  end
+
 end
