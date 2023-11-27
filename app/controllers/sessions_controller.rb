0984d9eef771902b23eec527a6f94f53279d94b8
Olawale <henry.williams658@gmail.com>
2023-11-27 04:13:01 +0000

    Add styling to pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,19 +6,22 @@ class SessionsController < ApplicationController
     @user = User.find_by_email(params[:email])
 
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      redirect_to '/'
+      session[:user_id] = @user.id
+      redirect_to '/', notice: "Logged in successfully."
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
+      # redirect_to '/login'
+      flash.now[:alert] = "Invalid email or password."
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to '/login', notice: "Logged out successfully."
   end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:product).permit(
+    params.require(:user).permit(
       :first_name,
       :last_name,
       :email,
